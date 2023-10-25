aa7e7f65bc1058ed56b7eedaf2c80a2380504c6b
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-10-25 20:05:27 +0100

    feat: Add Game class

---
+++ EnchantaPlay/Game.cs
@@ -0,0 +1,13 @@
+﻿namespace EnchantaPlay {
+    public class Game {
+        public string title { get; private set; }
+        public string description { get; private set; }
+        public string image { get; private set; }
+
+        public Game(string title, string description, string image) { 
+            this.title = title;
+            this.description = description;
+            this.image = image;
+        }
+    }
+}
