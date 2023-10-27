9ac3a7a40d0be349643d86d1ec61de300b29ca79
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-10-27 20:41:13 +0100

    fix: Modify spacing and small things that caused problems parcing json

--- EnchantaPlay/Game.cs
+++ EnchantaPlay/Game.cs
@@ -2,12 +2,12 @@
     public class Game {
         public string Title { get; private set; }
         public string Description { get; private set; }
-        public string Image { get; private set; }
+        public string Draw { get; private set; }
 
-        public Game(string title, string description, string image) { 
+        public Game(string title, string description, string draw) { 
             this.Title = title;
             this.Description = description;
-            this.Image = image;
+            this.Draw = draw;
         }
     }
 }
--- EnchantaPlay/GameCase.cs
+++ EnchantaPlay/GameCase.cs
@@ -8,22 +8,22 @@
 
         public override string ToString() {
             int titlePadding = GetPaddingLength(Game.Title);
-            string stringCase = "_________________________________________\n";
-            stringCase += "/                                       \n";
+            string stringCase = "_____________________________________\n";
+            stringCase += "/                                   \\\n";
             stringCase += "|" + new string(' ', titlePadding) + Game.Title + new string(' ', titlePadding) + "|\n";
-            stringCase += "|                                        |\n";
+            stringCase += "|                                   |\n";
 
-            string[] imageArray = Game.Image.Split(".");
+            string[] imageArray = Game.Draw.Split(";");
             for (int i = 0; i < imageArray.Length; i++) 
-                stringCase += $"|          {imageArray[i]}          |\n";
-            stringCase += "|                                        |\n";
-            stringCase += $"|      Platform: []                   |\n";
-            stringCase += $"|      Genre:    []                   |\n";
-            stringCase += $"|      Rating:   []                   |\n";
-            stringCase += $"|      Players:  []                   |\n";
-            stringCase += $"|      Release:  []                   |\n";
-            stringCase += "|                                        |\n";
-            stringCase += "\\_________________________________________/\n";
+                stringCase += $"|         {imageArray[i]}        |\n";
+            stringCase += "|                                   |\n";
+            stringCase += $"|      Platform: []                 |\n";
+            stringCase += $"|      Genre:    []                 |\n";
+            stringCase += $"|      Rating:   []                 |\n";
+            stringCase += $"|      Players:  []                 |\n";
+            stringCase += $"|      Release:  []                 |\n";
+            stringCase += "|                                   |\n";
+            stringCase += "\\___________________________________/\n";
             return stringCase;
 
             // ________________________________________
@@ -48,7 +48,7 @@
             // \______________________________________/
         }
         private int GetPaddingLength(string chain) {
-            return (40 - chain.Length) / 2;
+            return (36 - chain.Length) / 2;
         }
     }
 }
