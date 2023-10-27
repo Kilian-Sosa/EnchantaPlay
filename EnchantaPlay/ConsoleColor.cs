0d6bd6d1decd54e9be667f853fcc0b5e86ac219c
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-10-27 20:41:38 +0100

    feat: Change enum AnsiColor to static class

--- EnchantaPlay/ConsoleColor.cs
+++ EnchantaPlay/ConsoleColor.cs
@@ -1,9 +1,9 @@
 ﻿namespace EnchantaPlay {
-    public enum AnsiColor {
-        Reset = 0,
-        Red = 91,
-        Green = 92,
-        Bold = 1,
-        Blue = 94
+    public static class AnsiColor {
+        public static string Reset { get; } = "\u001b[0m";
+        public static string Red { get; } = "\u001b[91m";
+        public static string Green { get; } = "\u001b[92m";
+        public static string Bold { get; } = "\u001b[1m";
+        public static string Blue { get; } = "\u001b[94m";
     }
 }
