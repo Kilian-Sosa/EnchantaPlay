960bc48c10c0defab13130ec2c8a85c97cd7b9ea
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-10-31 18:24:47 +0000

    feat: Add new 'EnchantaPlay' logo

--- EnchantaPlay/Program.cs
+++ EnchantaPlay/Program.cs
@@ -7,6 +7,15 @@ namespace EnchantaPlay {
         static List<Game> Games = new List<Game>();
         static List<GameCase> GameCases = new List<GameCase>();
         static string gamesPath = "games.json";
+        static string logoString = @"
+                ___________             .__                   __        __________.__                
+                \_   _____/ ____   ____ |  |__ _____    _____/  |______ \______   \  | _____  ___.__.
+                 |    __)_ /    \_/ ___\|  |  \\__  \  /    \   __\__  \ |     ___/  | \__  \<   |  |
+                 |        \   |  \  \___|   Y  \/ __ \|   |  \  |  / __ \|    |   |  |__/ __ \\___  |
+                /_______  /___|  /\___  >___|  (____  /___|  /__| (____  /____|   |____(____  / ____|
+                        \/     \/     \/     \/     \/     \/          \/                   \/\/     
+            ";
+
         public static void Main(string[] args) {
             Console.OutputEncoding = System.Text.Encoding.UTF8; // For the card symbols to work
             PrintIntro();
@@ -14,13 +23,14 @@ namespace EnchantaPlay {
         }
 
         private static void PrintIntro() {
+            ShowHeader();
             Console.WriteLine("Welcome to EnchantaPlay!");
-            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Press any key to continue..."); 
             Console.ReadKey();
         }
 
         private static void PrintOptions() {
-            Console.Clear();
+            ShowHeader();
             Console.WriteLine("What would you like to do?");
             Console.WriteLine("1. Play a game");
             Console.WriteLine("2. Develop your own game (coming soon)");
@@ -67,7 +77,7 @@ namespace EnchantaPlay {
         }
 
         private static void ShowGames() {
-            Console.Clear();
+            ShowHeader();
 
             for (int i = 0; i < 20; i++) {
                 foreach (var gameCase in GameCases) {
@@ -77,6 +87,11 @@ namespace EnchantaPlay {
                 Console.WriteLine();
             }
             Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void ShowHeader() {
+            Console.Clear();
+            Console.WriteLine(logoString);
         }
 
         private static void Exit() {
